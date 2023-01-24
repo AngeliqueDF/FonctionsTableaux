8bfeaebe5a5d86bcc2d0ff2867f5ceb1d9d0cceb
angelique-df <faye.angelique.d@gmail.com>
2023-01-24 16:37:43 +0000

    Add `nbOccurrences`

--- src/FonctionsTableaux.java
+++ src/FonctionsTableaux.java
@@ -1,4 +1,13 @@
 public class FonctionsTableaux {
+    public static int nbOccurrences(char car, char[] tableau){
+        int res = 0;
+        for (int i = 0; i < tableau.length; i++) {
+            if(tableau[i] == car)
+                res++;
+        }
+        return res;
+    }
+
     public static boolean estDansTableau(char car, char[] tableau){
         for (int i = 0; i < tableau.length; i++) {
             if(tableau[i] == car)
