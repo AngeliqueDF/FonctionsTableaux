af11b32c337ff19ef90ac8e7d76983cebf3ffcde
angelique-df <faye.angelique.d@gmail.com>
2023-01-24 16:37:21 +0000

    Add `estDansTableau`

--- src/FonctionsTableaux.java
+++ src/FonctionsTableaux.java
@@ -1,2 +1,9 @@
 public class FonctionsTableaux {
+    public static boolean estDansTableau(char car, char[] tableau){
+        for (int i = 0; i < tableau.length; i++) {
+            if(tableau[i] == car)
+                return true;
+        }
+        return false;
+    }
 }
