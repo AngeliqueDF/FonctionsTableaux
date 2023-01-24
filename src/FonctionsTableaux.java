96bbfdf84e27285858b09891851d9a290c8dfff4
angelique-df <faye.angelique.d@gmail.com>
2023-01-24 16:37:50 +0000

    Add `includesArray`

--- src/FonctionsTableaux.java
+++ src/FonctionsTableaux.java
@@ -1,4 +1,15 @@
 public class FonctionsTableaux {
+    public static boolean includesArray(char[] tableau1, char[] tableau2){
+        for (int i = 0; i < tableau1.length; i++) {
+            for (int j = 0; j < tableau2.length; j++) {
+                if(nbOccurrences(tableau1[i], tableau2) == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static int nbOccurrences(char car, char[] tableau){
         int res = 0;
         for (int i = 0; i < tableau.length; i++) {
