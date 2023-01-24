075c850d227d08bc7105c1feec80f52fc24f65bc
angelique-df <faye.angelique.d@gmail.com>
2023-01-24 16:37:54 +0000

    test methods

--- src/FonctionsTableaux.java
+++ src/FonctionsTableaux.java
@@ -1,4 +1,15 @@
 public class FonctionsTableaux {
+    public static void main(String[] args) {
+        char[] testTab1 = {'a','b', 'c'};
+        char[] testTab2 = {'a','b', 'c', 'd', 'e','f'};
+        char[] testTab3 = {'a','b', 'c', 'd','b','a','b','d'};
+        System.out.println(estDansTableau('z',testTab1));
+        System.out.println(nbOccurrences('a',testTab1));
+        System.out.println(includesArray(testTab1, testTab2));
+        System.out.println(includesArray(testTab1, testTab3));
+        System.out.println(includesArray(testTab2, testTab3));
+    }
+
     public static boolean includesArray(char[] tableau1, char[] tableau2){
         for (int i = 0; i < tableau1.length; i++) {
             for (int j = 0; j < tableau2.length; j++) {
